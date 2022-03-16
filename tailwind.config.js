6ad678dc6da774b04b7e2fe29b7436cd04ec37e2
iamist <vicentechristian@gmail.com>
2022-03-17 03:08:49 +0800

    adding tailwind css and custom configuration

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      header: ['Raleway', "sans-serif"],
+      body: ['Open Sans', "sans-serif"]
+    },
+
+    container: {
+        center: true,
+        padding: "1rem"
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
