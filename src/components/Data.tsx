f6560db17f36c322412df5d7272a9ff01c4b3b14
iamist <vicentechristian@gmail.com>
2022-03-19 23:33:48 +0800

    addding home page data and layout change

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,12 +1,12 @@
 export const experiences = [
     {
         date: {
-            from: '2015',
+            from: 'June 2015',
             to: 'Present'
         },
         company: 'RCG Global Services',
         position: 'Consultant L3 - Senior Web Developer',
-        summary: 'Worked as UI web developer for Walt Disney\'s eCommerce website.',
+        summary: 'Worked as a lead UI web developer for Disney Visa Instant Credit - part of Walt Disney’s eCommerce website.',
         tech: [
             'Git, Jenkins, Jira, ServiceNow, AWS, PHP, Zend Framework, Angularjs, TypeScript,' +
             'Confluence, Splunk, AppDynamics, NodeJs, Docker, Java, Postman, Jmeter'
@@ -14,22 +14,117 @@ export const experiences = [
     },
     {
         date: {
-            from: '2015',
-            to: '2013'
+            from: 'Sep 2013',
+            to: 'March 2015'
         },
         company: 'LENDDO Lending Corp. Philippines',
         position: 'Software Engineer',
-        summary: 'Worked on different codebase, frameworks and tools for the development and implementation' +
+        summary: 'Worked on different codebase, frameworks and tools for the development and implementation of product features for lenddo.com' +
             'of product features for lenddo.com',
         tech: [
             'LithiumPHP, Symfony2, NodeJS, MongoDB, MySQL, AngularJS, BackboneJS, Mercurial, Redis, Apache2,' +
             'Nginx, Linux'
         ]
     },
-
+    {
+        date: {
+            from: 'July 2012',
+            to: 'Aug 2013'
+        },
+        company: 'RCG Global Services',
+        position: 'Senior PHP Developer',
+        summary: 'Worked as software consultant for Walt Disney’s intranet application. Helped improved application performance and user experience by reviewing and optimizing code.',
+        tech: [
+            'Zend® Server, Zend® Framework 1, IBM ClearCase®, AntHill®, Memcached'
+        ]
+    },
+    {
+        date: {
+            from: 'July 2009',
+            to: 'July 2012'
+        },
+        company: 'US Autoparts Network, Inc.',
+        position: 'Senior PHP Developer',
+        summary: 'Worked as a fullstack PHP frontend/backend developer for www.automd.com.',
+        tech: [
+            'Zend® Framework, SVN, TracTM, PHP/MySQL, ApacheTM Solr, Linux, Memcached, CruiseControl, Apache2'
+        ]
+    },
+    {
+        date: {
+            from: 'Sept 2005',
+            to: 'July 2009'
+        },
+        company: 'Webfocus.ph',
+        position: 'Senior Web Developer',
+        summary: 'Involved in development of various projects. Mentored junior developers. Led the development of building modular based libraries used to build website for clients.',
+        tech: [
+            'ASP3, PHP/MySQL, ApacheTM, IIS, Memcache, MsAccess, MsSQL, Macromedia Fireworks, Flash, Adobo Photoshop'
+        ]
+    },
 ];
 
 export const techStacks = [
+    {
+        title: 'NodeJs, PHP, Javascript, CSS, HTML',
+        progressLabel: 'Excelent',
+        progress: 90
+    },
+    {
+        title: 'MySQL',
+        progressLabel: 'Excelent',
+        progress: 90
+    },
+    {
+        title: 'Symfony2, ZendFramework 1',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'AngularJs, BackboneJs, JQuery',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'Mocha, Chai, Jasmine, Sinon, PHPUnit',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'LESS, Sass, Webpack, Grunt, NPM, Yarn',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'MacOS, Windows, Linux',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'Apache2, Nginx',
+        progressLabel: 'Very Good',
+        progress: 75
+    },
+    {
+        title: 'AWS, Docker, Jenkins, AppDynamics, Splunk, Jmeter',
+        progressLabel: 'Good',
+        progress: 50
+    },
+    {
+        title: 'Laravel, CodeIgniter',
+        progressLabel: 'Good',
+        progress: 50
+    },
+    {
+        title: 'Java',
+        progressLabel: 'Good',
+        progress: 50
+    },
+    {
+        title: 'MongoDB, Redis, Memcached',
+        progressLabel: 'Good',
+        progress: 50
+    },
     {
         title: 'ReactJs',
         progressLabel: 'Good',
@@ -39,10 +134,5 @@ export const techStacks = [
         title: 'Angular',
         progressLabel: 'Good',
         progress: 50
-    },
-    {
-        title: 'Javascript',
-        progressLabel: 'Very Good',
-        progress: 90
     }
 ];
--- src/components/TechSkill.tsx
+++ src/components/TechSkill.tsx
@@ -15,14 +15,18 @@ const TechSkiils : React.FC<IProps> = ({techStacks}) => {
     return (
         <div className="w-10/12 md:w-10/12 lg:6/12 mx-auto relative pb-20">
         <h2 className="text-3xl text-left font-bold text-blue-500">Technical Stacks/Skills</h2>
-        {
-            techStacks.map( tech => {
-                return (
-                    <ProgressBar {...tech}>
-                    </ProgressBar>
-                )
-            })
-        }
+            <div className="grid grid-cols-4 gap-4">
+            {
+                techStacks.map( tech => {
+                    return (
+                        <div className="flex flex-col">
+                            <ProgressBar {...tech}>
+                            </ProgressBar>
+                        </div>
+                    )
+                })
+            }
+            </div>
         </div>
     )
 }
